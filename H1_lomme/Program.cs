07ad469ddffed2660535832a564da54e2bf870d3
Jacob Ørnskov Andersen <jacobvoandersen@gmail.com>
2023-02-27 16:48:08 +0100

    Tried somethings

--- H1_lomme/Program.cs
+++ H1_lomme/Program.cs
@@ -1,36 +1,49 @@
 ﻿
-int Tal1 = Convert.ToInt32(GetInput("tal1"));
-int Tal2 = Convert.ToInt32(GetInput("tal1"));
+//int Tal1 = Convert.ToInt32(GetInput("tal1"));
+//int Tal2 = Convert.ToInt32(GetInput("tal1"));
 
-string[] ope = new string[] { "+" };
 
-var Input = GetInput(""" 
-    Chose a fun thing:
-    1: Addition
-    2: Subtraction
-    3: Multiplication
-    4: Division
-    """);
+//var Input = GetInput(""" 
+//    Chose a fun thing:
+//    1: Addition
+//    2: Subtraction
+//    3: Multiplication
+//    4: Division
+//    """);
 
-switch (Input)
-{
-    case "+":
-        char op = Convert.ToChar(Input);
-        int res = (Tal1 + op + Tal2); 
-        Console.WriteLine(op);
-        break;
-    case "2":
-        Console.WriteLine(Tal1 - Tal2);
-        break;
-    case "3":
-        Console.WriteLine(Tal1 * Tal2);
-        break;
-    case "4":
-        Console.WriteLine(Tal1 / Tal2);
-        break;
-    default:
-        Console.WriteLine("Invalid input");
-        break;
+
+Calc(GetInput("Input first number: "), GetInput("Input second number: "), GetInput("+ | - | * | /"));
+
+void Calc(string numb1, string ope, string numb2)
+{ //Uses two arrays of different types, 1 for operators and another for the results from each
+    string[] op = { "+", "-", "*", "/" };
+    double[] res = new double[4];
+    //The inputs after seeing if they are numbers
+    double inp1 = 0;
+    double inp2 = 0;
+    //Where the inputs are checked to see if they are numbers
+    double.TryParse(numb1, out inp1);
+    double.TryParse(numb2, out inp2);
+    //Where the math happens and getting added to their personalized housing
+
+    string result = "";
+    for (int i = 0; i < op.Length; i++)
+    {
+        if (op[i] == ope)
+        {
+            result = (numb1 + op[i].OfType(operator) + numb2);
+            return;
+        }
+    }
+    Console.WriteLine(result);
+    //res[0] = inp1 + inp2;
+    //res[1] = inp1 - inp2;
+    //res[2] = inp1 * inp2;
+    //res[3] = inp1 / inp2;
+
+    //Console.WriteLine("-------------------------------");
+    //for (int i = 0; i < op.Length; i++) //Prints out every equation
+    //    Console.WriteLine($"{numb1} {op[i]} {numb2} = {res[i]}");
 }
 
 Console.ReadKey();
@@ -38,6 +51,6 @@ Console.ReadKey();
 
 string GetInput(string text)
 {
-    Console.WriteLine(text);
+    Console.Write(text);
     return Console.ReadLine();
 }
