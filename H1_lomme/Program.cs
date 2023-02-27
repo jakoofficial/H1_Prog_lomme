f2c51ccadf66518c647cf9fe41a4034d4692c6ce
Jaco296c <jaco296c@outlook.dk>
2023-02-27 13:12:07 +0100

    Epicness
    Yes this is good

--- H1_lomme/Program.cs
+++ H1_lomme/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int Tal1 = Convert.ToInt32(GetInput("tal1"));
+int Tal2 = Convert.ToInt32(GetInput("tal1"));
+
+
+
+var Input = GetInput(""" 
+    Chose a fun thing:
+    1: Addition
+    2: Subtraction
+    3: Multiplication
+    4: Division
+    """);
+
+switch (Input)
+{
+    case "1":
+        Console.WriteLine(Tal1 + Tal2);
+        break;
+    case "2":
+        Console.WriteLine(Tal1 - Tal2);
+        break;
+    case "3":
+        Console.WriteLine(Tal1 * Tal2);
+        break;
+    case "4":
+        Console.WriteLine(Tal1 / Tal2);
+        break;
+    default:
+        Console.WriteLine("Invalid input");
+        break;
+}
+
+string GetInput(string text)
+{
+    Console.WriteLine(text);
+    return Console.ReadLine();
+}
